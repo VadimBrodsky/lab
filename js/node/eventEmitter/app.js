ee1d213d76b4b9c286de92a06dfce333e739bb40
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-12-04 21:08:52 +0000

    Add Node core event emitter, remove magic strings

--- js/node/eventEmitter/app.js
+++ js/node/eventEmitter/app.js
@@ -1,17 +1,23 @@
-const Emitter = require('./emitter');
-const emtr = new Emitter();
+const CustomEmitter = require('./emitter');
+const NodeEmitter = require('events');
+
+// configuration to avoid magic strings
+const eventConfig = require('./config').events;
+
+const emtr = new CustomEmitter();
+// const emtr = new NodeEmitter();
 
 // Set the first event listener
-emtr.on('greet', () => {
+emtr.on(eventConfig.GREET, () => {
   console.log('Somewhere, someone said Aloha.');
 });
 
 // Set the second event listener
-emtr.on('greet', () => {
+emtr.on(eventConfig.GREET, () => {
   console.log('A greeting occured!');
 });
 
 
 // Event happened, fire emitter
 console.log('Hello!');
-emtr.emit('greet');
+emtr.emit(eventConfig.GREET);
