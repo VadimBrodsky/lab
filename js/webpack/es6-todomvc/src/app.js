c787c30340ff01b8c81b4fd1035a8c6d09480ead
Vadim Brodsky <vadim.brodsky@vidyard.com>
2016-12-09 15:54:45 -0500

    Convert to ES6 imports

--- js/webpack/es6-todomvc/src/app.js
+++ js/webpack/es6-todomvc/src/app.js
@@ -1,12 +1,11 @@
-// Require the CSS as any other JS module
-import 'todomvc-app-css/index.css';
+import 'todomvc-app-css/index.css'
 
-import View from './view';
-import {log} from './helpers';
-import Controller from './controller';
-import Model from './model';
-import Store from './store';
-import Template from './template';
+import View from './view'
+import {log} from './helpers'
+import Controller from './controller'
+import Model from './model'
+import Store from './store'
+import Template from './template'
 
 /**
  * Sets up a brand new Todo list.
@@ -14,15 +13,15 @@ import Template from './template';
  * @param {string} name The name of your new to do list.
  */
 function Todo(name) {
-  this.storage = new app.Store(name);
-  this.model = new app.Model(this.storage);
-  this.template = new app.Template();
-  this.view = new app.View(this.template);
-  this.controller = new app.Controller(this.model, this.view);
+  this.storage = new Store(name)
+  this.model = new Model(this.storage)
+  this.template = new Template()
+  this.view = new View(this.template)
+  this.controller = new Controller(this.model, this.view)
 }
 
-export function onLoad() {
-  const todo = new Todo('todos-vanillajs');
-  todo.controller.setView(document.location.hash);
-  log('view set');
+export function onLoad() { // eslint-disable-line import/prefer-default-export
+  const todo = new Todo('todos-vanillajs')
+  todo.controller.setView(document.location.hash)
+  log('view set')
 }
