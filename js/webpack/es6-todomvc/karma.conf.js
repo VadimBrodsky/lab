a940d08c1cbb7fcc386f912eb274a3b85f6c76dc
Vadim Brodsky <vadim.brodsky@vidyard.com>
2016-12-09 14:47:58 -0500

    Preprocess the Karma test with Webpack

--- js/webpack/es6-todomvc/karma.conf.js
+++ js/webpack/es6-todomvc/karma.conf.js
@@ -1,11 +1,17 @@
+const webpackEnv = {env: 'test'};
+const webpackConfig = require('./webpack.config')(webpackEnv);
+const fileGlob = 'src/**/*.test.js';
+
 module.exports = config => {
   config.set({
     basePath: '',
     frameworks: ['mocha', 'chai'],
-    files: [
-      'src/**/*.test.js',
-    ],
+    files: [fileGlob],
     reporters: ['progress'],
+    preprocessors: {
+      [fileGlob]: ['webpack']
+    },
+    webpack: webpackConfig,
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
--- js/webpack/es6-todomvc/src/controller.test.js
+++ js/webpack/es6-todomvc/src/controller.test.js
@@ -1,8 +1,13 @@
+const Controller = require('./controller');
+
 describe('test', () => {
   it('works', () => {
     expect(true).to.be.true;
   });
   it('works again', () => {
     expect('hi').to.equal('hi');
+  });
+  it('has a controller', () => {
+    expect(Controller).to.exist;
   });
 });
