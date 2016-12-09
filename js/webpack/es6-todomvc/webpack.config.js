4f5dc66845dc6a526d25263266cf964372491e7d
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-12-09 03:43:06 +0000

    Specify webpack entry

--- js/webpack/es6-todomvc/webpack.config.js
+++ js/webpack/es6-todomvc/webpack.config.js
@@ -1,5 +1,9 @@
+const resolve = require('path').resolve;
+
 module.exports = () => {
   return {
+    context: resolve('src'),
+    entry: './bootstrap.js',
     output: {
       filename: 'bundle.js',
     },
