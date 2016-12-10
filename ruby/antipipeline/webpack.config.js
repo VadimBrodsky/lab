fcbb602f02f9b086db89e31708730acabdf77e10
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-12-10 23:23:51 +0000

    Add initial webpack config

---
+++ ruby/antipipeline/webpack.config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const webpack = require('webpack');
+
+module.exports = {
+  context: `${__dirname}/app/assets/javascripts`,
+  entry: {
+    application: './application.js',
+  },
+  output: {
+    path: `${__dirname}/public`,
+    filename: 'javascripts/[name].js',
+  },
+};
