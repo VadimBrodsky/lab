ec5be9b1b5b67cbb848010c7a324178733d7428d
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-12-26 02:39:53 +0000

    Add react-typescript-webpack example

---
+++ js/react/typescript-react-webpack/webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+var config = {
+  entry: ['./src/app.tsx'],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/build/',
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/
+      }
+    ]
+  }
+};
+
+module.exports = config;
