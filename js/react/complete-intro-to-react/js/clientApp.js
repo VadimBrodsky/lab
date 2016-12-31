71c2eb1563f57e47c541dcdd989a5d784d16323d
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-12-31 02:41:44 +0000

    Add color prop

--- js/react/complete-intro-to-react/js/clientApp.js
+++ js/react/complete-intro-to-react/js/clientApp.js
@@ -6,9 +6,12 @@ var h1 = React.DOM.h1;
 // createClass - creates a new type of element
 // composite component
 var MyTitle = React.createClass({
+  // all react components need to have a render method
+  // it must return a component
+  // needs to be a pure function
   render: function() {
     return (
-      div(null,
+      div({style: {color: this.props.color}},
         h1(null, this.props.title)
       )
     );
@@ -23,10 +26,10 @@ var MyTitleFact = React.createFactory(MyTitle);
 // createElement - creates an instance of the class
 var MyFirstComponent = (
   div(null,
-    React.createElement(MyTitle, {title: 'Game of Thrones'}),
-    React.createElement(MyTitle, {title: 'Clash of Kings'}),
-    React.createElement(MyTitle, {title: 'Storm of Swords'}),
-    MyTitleFact({title: 'Feast for Crows'})
+    React.createElement(MyTitle, {title: 'Game of Thrones', color: 'rebeccapurple'}),
+    React.createElement(MyTitle, {title: 'Clash of Kings', color: 'mediumaquamarine'}),
+    React.createElement(MyTitle, {title: 'Storm of Swords', color: 'peru'}),
+    MyTitleFact({title: 'Feast for Crows', color: 'papayawhip'})
   )
 );
 
