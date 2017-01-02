f31e9fd5c20ed2448e5096d9e078d852c85e58a0
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-01-02 17:49:23 +0000

    Return movie titles

--- js/react/complete-intro-to-react/js/Search.jsx
+++ js/react/complete-intro-to-react/js/Search.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import shows from '../public/data';
+import data from '../public/data';
 
 const Search = () => (
-  <pre><code>
-    { JSON.stringify(shows, null, 4) }
-  </code></pre>
+  <div className='container'>
+    {data.shows.map((show) => {
+      return <h3>{show.title}</h3>
+    })}
+  </div>
 );
 
 export default Search;
