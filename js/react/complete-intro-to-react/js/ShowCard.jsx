0224ced73a293cb3642e527ff0a24b694cdd8e3c
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-01-05 02:38:26 +0000

    Add prop types for better validation

--- js/react/complete-intro-to-react/js/ShowCard.jsx
+++ js/react/complete-intro-to-react/js/ShowCard.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 
 const ShowCard = (props) => (
   <div className='show-card'>
-    <img src={`public/img/posters/${props.show.poster}`} className='show-card-img' />
+    <img src={`public/img/posters/${props.poster}`} className='show-card-img' />
     <div className='show-card-text'>
-      <h3 className='show-card-title'>{props.show.title}</h3>
-      <h4 className='show-card-year'>({props.show.year})</h4>
-      <p className='show-card-description'>{props.show.description}</p>
+      <h3 className='show-card-title'>{props.title}</h3>
+      <h4 className='show-card-year'>({props.year})</h4>
+      <p className='show-card-description'>{props.description}</p>
     </div>
   </div>
 );
+
+// Type hinting for props
+ShowCard.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  description: React.PropTypes.string.isRequired,
+  year: React.PropTypes.string.isRequired,
+  poster: React.PropTypes.string.isRequired
+};
 
 export default ShowCard;
