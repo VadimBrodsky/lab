957104409868ccae25cf97fb21566a05524d5b35
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-01-08 15:30:34 -0500

    Setup project + run somketest

---
+++ js/react/hubtags/src/app.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+interface HelloProps {
+  name: string
+}
+
+class Hello extends React.Component<HelloProps, {}> {
+  render () {
+    return <div>Hello, {this.props.name}</div>
+  }
+}
+
+ReactDom.render(<Hello name="Vadim"/>, document.getElementById('app'));
---
+++ js/react/hubtags/webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+var config = {
+  entry: ['./src/app.tsx'],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/build/',
+  },
+  devtool: 'source-map',
+  performance: {
+    hints: false
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/
+      }
+    ]
+  }
+};
+
+module.exports = config;
