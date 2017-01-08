12f164840fe88b3579520c62b749553ee9ca8aac
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-01-07 23:19:01 -0500

    Conver app component to statefull, add routing method to filter show

--- js/react/complete-intro-to-react/js/clientApp.jsx
+++ js/react/complete-intro-to-react/js/clientApp.jsx
@@ -11,14 +11,28 @@ import data from '../public/data';
 // const Router = ReactRouter.Router;
 // const { Router, Route, hashHistory } = ReactRouter;
 
-const App = () => (
-  <Router history={hashHistory}>
-    <Route path='/' component={Layout} >
-      <IndexRoute component={Landing} />
-      <Route path='/search' component={Search} shows={data.shows} />
-      <Route path='/details/:id' component={Details} />
-    </Route>
-  </Router>
-);
+const App = React.createClass((
+  assignShow (nextState, replace) {
+    const showArray = shows.filter( (show) => show.imdbID === nextState.id );
+
+    if (showArray.length < 1) {
+      return replace('/');
+    }
+
+    Object.assign(nextState.params, showArray[0]);
+    return nextState;
+  },
+  render () {
+    return (
+      <Router history={hashHistory}>
+        <Route path='/' component={Layout} >
+          <IndexRoute component={Landing} />
+          <Route path='/search' component={Search} shows={data.shows} />
+          <Route path='/details/:id' component={Details} onEnter={this.assignShow} />
+        </Route>
+      </Router>
+    );
+  }
+});
 
 ReactDOM.render(<App />, document.getElementById('app'));
