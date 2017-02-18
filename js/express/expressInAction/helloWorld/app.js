dfcd7ae26fc46feffa93f6ce5e7e62d0635e2028
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-02-18 13:18:28 -0500

    Add basic authenitcation middleware

--- js/express/expressInAction/helloWorld/app.js
+++ js/express/expressInAction/helloWorld/app.js
@@ -8,9 +8,19 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  const minute = (new Date()).getMinutes();
+  if ((minute % 2) === 0) {
+    next();
+  } else {
+    res.statusCode = 403;
+    res.end('Not authorized');
+  }
+});
+
 app.use((req, res) => {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Hello, World");
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello, World');
 });
 
 http.createServer(app).listen(3000);
