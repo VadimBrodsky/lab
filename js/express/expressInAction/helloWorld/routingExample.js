7b381d873bbe1cdf1bd2fb4c3af99c882e24f066
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-02-18 13:46:45 -0500

    Add routing example

---
+++ js/express/expressInAction/helloWorld/routingExample.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+  res.end('Welcome to my homepage!');
+});
+
+app.get('/about', (req, res) => {
+  res.end('Welcome to the about page!');
+});
+
+app.get('/weather', (req, res) => {
+  res.end('The current weather is NICE.');
+});
+
+app.use((req, res) => {
+  res.statusCode = 404;
+  res.end('404');
+});
+
+http.createServer(app).listen(3000);
