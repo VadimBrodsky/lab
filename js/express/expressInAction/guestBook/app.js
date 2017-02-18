ae15fec3f9acd415783c51e4361bd17652af2048
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-02-18 16:30:15 -0500

    Setup guesbook project

---
+++ js/express/expressInAction/guestBook/app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
