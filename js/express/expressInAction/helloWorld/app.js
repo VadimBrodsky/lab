8c9086c8ef9556b21208e1c1276e5eb863438c3f
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-02-18 13:03:23 -0500

    Add loggin middleware

--- js/express/expressInAction/helloWorld/app.js
+++ js/express/expressInAction/helloWorld/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const http = require('http');
 
 const app = express();
+
+app.use((req, res, next) => {
+  console.log(`In comes a ${req.method} to ${req.url}`);
+  next();
+});
+
 app.use((req, res) => {
   res.writeHead(200, {"Content-Type": "text/plain"});
   res.end("Hello, World");
