0f87f10a798c257949e46ff0a478fe74589b15d2
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-02-19 18:04:15 -0500

    Add example of error handling middleware

--- js/express/expressInAction/staticFileServer/app.js
+++ js/express/expressInAction/staticFileServer/app.js
@@ -5,28 +5,44 @@ const fs = require('fs');
 
 const app = express();
 
-// app.use((req, res, next) => {
-//   console.log(`Request IP: ${req.ip}`);
-//   console.log(`Request date: ${new Date()}`);
-//   next();
-// });
+/*
+app.use((req, res, next) => {
+  console.log(`Request IP: ${req.ip}`);
+  console.log(`Request date: ${new Date()}`);
+  next();
+});
+*/
 
 const logger = morgan('short');
 app.use(logger);
 
-// app.use((req, res, next) => {
-//   const filePath = path.join(__dirname, 'static', req.url);
-//   fs.exists(filePath, (exists) => {
-//     if (exists) {
-//       res.sendFile(filePath);
-//     } else {
-//       next();
-//     }
-//   });
-// });
+/*
+app.use((req, res, next) => {
+  const filePath = path.join(__dirname, 'static', req.url);
+  fs.exists(filePath, (exists) => {
+    if (exists) {
+      res.sendFile(filePath, (err) => {
+        if (err) {
+          // enter error more if can't read file
+          next(new Error('Error sending file!'));
+        }
+      });
+    } else {
+      next();
+    }
+  });
+});
+*/
 
+// express builtin static file middleware
 const staticPath = path.join(__dirname, 'static');
 app.use(express.static(staticPath));
+
+// middleware that logs all errors
+app.use((err, req, res, next) => {
+  console.log(err);
+  next(err);
+});
 
 app.use((req, res) => {
   res.status(404);
