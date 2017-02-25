59385eda13e547b5bb018d566c0c6fcdcb22f515
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-02-25 12:30:52 -0500

    Fix compilation errors, run through gofmt

--- go/echo1.go
+++ go/echo1.go
@@ -1,16 +1,16 @@
 // Echo1 prints its command line arguments, like the unix echo
 package main
 
-import {
-  "fmt"
-  "os"
-}
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-  var s, sep string
-  for i := 1; i < len(os.Args); i++ {
-    s += sep + os.Args[i]
-    sept = " "
-  }
-  fmt.Println(s)
+	var s, sep string
+	for i := 2; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
 }
