f1a4bd245884b05bb2a17247e099461019cde360
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-02-24 22:14:21 -0500

    Add basic mongoose model

---
+++ js/express/expressInAction/lamMongo/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const SALT_FACTOR = 10;
+
+const userSchema = mongoose.Schema({
+  username: { type: String, required: true, uniue: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  displayName: String,
+  bio: String
+});
+
