bcaa5f1bb1f2ca8d66705bd87309a07e5a73be16
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-02-24 22:32:07 -0500

    Add main app file

---
+++ js/express/expressInAction/lamMongo/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const flash = require('connect-flash');
+const routes = require('./routes');
+
+const app = express();
+
+mongooser.connect('mongodb://localhost:2717/test');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('short'));
+app.use(bodyParser.urlencoded({ enxtended: false }));
+app.use(cookieParser());
+app.use(session({
+  secret: 'TKRv01Js=HYqrvagQ#&!F!%V]Ww/4KiVs$s,<<MX',
+  resave: true,
+  savUninitialized: true
+}));
+app.use(flash());
+
+app.use(routes);
+
+app.listen(app.get('port'), () => {
+  console.log(`Server started on port ${app.get('port')}`);
+})
